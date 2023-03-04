f297c4cc046c382ab8f09af1c8fa73099ea2fdbc
MoinUddin005 <moin368892@gmail.com>
2023-03-04 20:58:35 +0600

    OwnStroy complited

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import OwnStroy from '../OwnStroy/OwnStroy';
 
 
 const Home = () => {
     return (
         <div>
-            
+            <OwnStroy></OwnStroy>
             
         </div>
     );
