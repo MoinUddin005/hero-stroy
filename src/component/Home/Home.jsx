0cf4a979e16305a9168be8303a0877861469e63d
MoinUddin005 <moin368892@gmail.com>
2023-03-04 23:25:56 +0600

    Subscribe complited

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import OwnStroy from '../OwnStroy/OwnStroy';
+import Subscribe from '../Subscribe/Subscribe';
 
 
 const Home = () => {
     return (
         <div>
             <OwnStroy></OwnStroy>
+            <Subscribe></Subscribe>
             
         </div>
     );
